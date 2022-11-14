4cddf398d3ab5d575731aada12a8b17ba3fd6794
lucas fabre <lucasfabre86@proton.me>
2022-11-14 12:07:52 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  GET  '/new', to: 'games#new'
+  POST '/score', to: 'games#score'
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
