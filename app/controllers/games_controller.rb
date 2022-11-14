0617d1aa5fcde3c36a0c6dbea283ea90c4b7f159
lucas fabre <lucasfabre86@proton.me>
2022-11-14 17:48:54 +0100

    better

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,24 +1,55 @@
+require "open-uri"
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @letters = ('A'..'Z').to_a.sample(10)
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
     @letters.shuffle!
-    # raise
   end
 
   def score
-    @word = params[:word]
-    @letters = params[:letters].delete(' ').downcase
-    @result = "sorry but TEST can't be built out of #{@letters}"
-    raise
-    # if @word != @letters
-    #   @result
-    # end
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
   end
 
-  # def included?(w, l)
-  #   w.join
-  #   l.each do |x|
-  #     x.belongs_to?(w)
-  #   end
-  # end
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
 end
+
+# class GamesController < ApplicationController
+#   def new
+#     @letters = ('A'..'Z').to_a.sample(10)
+#     @letters.shuffle!
+#     # raise
+#   end
+
+#   def score
+#     @word = params[:word]
+#     @letters = params[:letters].delete(' ').downcase
+#     @result = "sorry but TEST can't be built out of #{@letters}"
+#     raise
+#     # if @word != @letters
+#     #   @result
+#     # end
+#   end
+
+#   # def included?(w, l)
+#   #   w.join
+#   #   l.each do |x|
+#   #     x.belongs_to?(w)
+#   #   end
+#   # end
+# end
